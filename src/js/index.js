5997e44630ffac41ecfe636bfd15c27a2f994eba
AnzhelikaKlyushnikova <missangela01rus@icloud.com>
2024-03-28 12:10:28 +0300

    new

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+import mobileBurger from './modules/mobile-nav.js'
+mobileBurger()
+
+import mousEvent from './modules/mouseover-event.js'
+mousEvent()
+
+import './modules/slider.js'
+
+import './modules/photo-slider.js'
